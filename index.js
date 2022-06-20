5ef219b4d4f5b8d03752c9d9a71fcebb31e46c48
Walter Santos <waltercms14@gmail.com>
2022-06-20 09:13:47 -0100

    porta atualizado

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false}));
 const testRoutes = require('./routes/testdata');
 
 app.use(testRoutes );
