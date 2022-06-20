95880ccdd1a2580ace4ab1782db76388e5d88c99
Walter Santos <waltercms14@gmail.com>
2022-06-20 09:03:12 -0100

    porta atualizado

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const testRoutes = require('./routes/testdata');
 
 app.use(testRoutes );
 // prevent cors error on frontend
-const server = app.listen(3003, function () {
-   console.log("Server running at port 3003")
+const server = app.listen(80, function () {
+   console.log("Server running at port 80")
 })
 
 
