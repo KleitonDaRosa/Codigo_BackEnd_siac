565e189037bf344d239c56adcf0e014a89607714
Walter Santos <waltercms14@gmail.com>
2022-07-03 23:12:57 -0100

    porta atualizado

--- index.js
+++ index.js
@@ -2,11 +2,24 @@
 require('dotenv').config()
 const express = require('express');
 const pool = require('./config/database')
-const cors = require("cors")
 const port = process.env.PORT || 3003
 const app = express();
-app.use(cors());
 
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*")
+   res.header(
+       "Access-Control-Allow-Headers",
+       "Origin, X-Requested, Content-Type, Accept Authorization"
+   )
+   if (req.method === "OPTIONS") {
+      res.header(
+          "Access-Control-Allow-Methods",
+          "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+   }
+   next()
+})
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}));
